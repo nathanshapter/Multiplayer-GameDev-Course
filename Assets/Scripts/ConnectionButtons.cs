897deff1811417e23ab854d92b0baff493abe610
Nathan Shapter <shapternathan@protonmail.com>
2023-07-18 13:44:19 -0400

    open host and join button for both clients enabled

--- Assets/Scripts/ConnectionButtons.cs
+++ Assets/Scripts/ConnectionButtons.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class JoinServer : MonoBehaviour
+public class ConnectionButtons : MonoBehaviour
 {
     public void Join()
     {
         NetworkManager.Singleton.StartClient();
+    }
+    public void StartHost()
+    {
+        NetworkManager.Singleton.StartHost();
     }
 }
